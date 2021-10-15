f5acb7ee19d506edc24d8c765442572e8e75cb0c
Lucas Sepriano <88594847+LucasSepri@users.noreply.github.com>
2021-10-15 19:43:43 -0300

    Add files via upload

---
+++ Atividade C# calculo de raio/Atividade C# calculo de raio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double area, raio, total;
+
+            Console.Write("Digite raio: ");
+            raio = double.Parse(Console.ReadLine());
+            area = raio * raio * 3.14;
+            Console.Write(+ area);
+            Console.ReadKey();
+        }
+    }
+}
